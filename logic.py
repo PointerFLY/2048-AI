faf00b756f0bae30d8caf1e61f48c92cfa74f48f
PointerFLY <linghao.michael.ma@gmail.com>
2019-04-06 15:57:18 +0800

    Right action logic

--- logic.py
+++ logic.py
@@ -13,13 +13,38 @@ class Logic:
         self.matrix_updated: callable = None
 
         self.row_count = 4
-        self.matrix = [[0 for _ in range(self.row_count)] for _ in range(self.row_count)]
+        self.matrix = [[2 for _ in range(self.row_count)] for _ in range(self.row_count)]
 
     def perform_action(self, action: Action):
-        # merge if possible
-        self.generate_next()
+        self._right()
         self.matrix_updated(self.matrix)
+        print(self.matrix)
 
-    def generate_next(self):
-        # randomly generate
+    def _right(self):
+        i = 0
+        while i < self.row_count:
+            j = self.row_count - 2
+            while j >= 0:
+                if self.matrix[i][j] != 0 and self.matrix[i][j] == self.matrix[i][j+1]:
+                    self.matrix[i][j+1] += self.matrix[i][j]
+                    self.matrix[i][j] = 0
+                    j -= 1
+                j -= 1
+
+            zero_index = None
+            j = self.row_count - 1
+            while j > 0:
+                if self.matrix[i][j] == 0:
+                    if not zero_index:
+                        zero_index = j
+                elif zero_index:
+                    self.matrix[i][zero_index] = self.matrix[i][j]
+                    self.matrix[i][j] = 0
+                    zero_index -= 1
+                j -= 1
+
+            i += 1
+
+    def _generate_next(self):
+        # randomly generate1
         pass
