05acaeb42a114f9fd3221bdacde2da3009fbbb5f
PointerFLY <linghao.michael.ma@gmail.com>
2019-11-20 21:10:35 +0800

    Remove learning agent

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from game import Game
 from human import HumanAgent
 from stochastic import StochasticAgent
-from greedy import GreedyAgent
 from expectimax import ExpectimaxAgent
-# from learning import LearningAgent
+from stochastic import StochasticAgent
 import sys
 
 sys.setrecursionlimit(10000)
