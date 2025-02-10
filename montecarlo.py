2ba1c3c1c966606646384549775b363b59e58a7a
Michael Ma <linghao.michael.ma@gmail.com>
2025-02-10 10:59:06 +0800

    Optimize montercarlo

--- montecarlo.py
+++ montecarlo.py
@@ -1,32 +1,62 @@
 from agent import Agent
 from state import State, Action
 from random import randrange
+import multiprocessing as mp
+from time import time
+from functools import lru_cache
+import numpy as np
 
-_SAMPLE_PER_ACTION = 100
+_TIME_LIMIT = 0.25  # seconds
+_CONFIDENCE_THRESHOLD = 0.95
 
 
 class MonteCarloAgent(Agent):
+    def __init__(self, state):
+        super().__init__(state)
+        self.pool = mp.Pool(processes=mp.cpu_count())
+
     def next_action(self):
         legal_actions = self.state.legal_actions()
         if not legal_actions:
             return None
 
-        best_action = max(legal_actions, key=self.sample)
-        return best_action
+        start_time = time()
+        action_scores = {action: [] for action in legal_actions}
 
-    def sample(self, action) -> float:
-        total_score = 0
-        for i in range(_SAMPLE_PER_ACTION):
-            state = self.state.copy()
-            state.perform_action(action)
+        while time() - start_time < _TIME_LIMIT:
+            samples = [(self.state.copy(), action) for action in legal_actions]
+            results = self.pool.starmap(self._sample_single, samples)
+
+            for action, score in zip(legal_actions, results):
+                action_scores[action].append(score)
+
+            # Early termination if we have a clear winner
+            if (
+                len(action_scores[legal_actions[0]]) > 30
+            ):  # Minimum samples before checking
+                scores = [np.mean(scores) for scores in action_scores.values()]
+                max_score = max(scores)
+                confidence = sum(1 for s in scores if max_score - s > max_score * 0.1)
+                if confidence / len(scores) >= _CONFIDENCE_THRESHOLD:
+                    break
+
+        return max(legal_actions, key=lambda a: np.mean(action_scores[a]))
+
+    @staticmethod
+    def _sample_single(state, action) -> float:
+        state.perform_action(action)
+        return MonteCarloAgent._simulate(state)
+
+    @staticmethod
+    @lru_cache(maxsize=10000)
+    def _simulate(state) -> float:
+        legal_actions = state.legal_actions()
+        while legal_actions:
+            idx = randrange(len(legal_actions))
+            state.perform_action(legal_actions[idx])
             legal_actions = state.legal_actions()
-            while legal_actions:
-                idx = randrange(len(legal_actions))
-                act = legal_actions[idx]
-                state.perform_action(act)
-                legal_actions = state.legal_actions()
+        return state.score
 
-            total_score += state.score
-
-        score = total_score / _SAMPLE_PER_ACTION
-        return score
+    def __del__(self):
+        self.pool.close()
+        self.pool.join()
