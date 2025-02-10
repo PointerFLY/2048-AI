e5c814ba4bb40b4be9b82992233085d8d8dee2b4
Michael Ma <linghao.michael.ma@gmail.com>
2025-02-10 10:21:29 +0800

    Remove usused files

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from game import Game
 from human import HumanAgent
 from stochastic import StochasticAgent
 from expectimax import ExpectimaxAgent
-from stochastic import StochasticAgent
 from monte_carlo import MonteCarloAgent
 from mcts import MonteCarloTreeSearchAgent
 import sys
